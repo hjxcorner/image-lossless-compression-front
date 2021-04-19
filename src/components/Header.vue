e372a8904f1b4f42a9aee4a329bf1237ad4b951e
h-corner <157517941@qq.com>
2021-04-19 15:32:17 +0800

    退出登录

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
     <el-menu
-      default-active="1"
+      :default-active="defaultActive"
       class="header-menu"
       mode="horizontal"
       @select="handleSelect"
@@ -9,26 +9,73 @@
       active-text-color="#409EFF"
     >
       <router-link
-        to="/"
+        to="/compression"
         tag="div"
       >
-        <el-menu-item index="1">图片压缩</el-menu-item>
+        <el-menu-item index="0">图片压缩</el-menu-item>
       </router-link>
       <router-link
         to="/conversion"
         tag="div"
       >
-        <el-menu-item index="2">格式转换</el-menu-item>
+        <el-menu-item index="1">格式转换</el-menu-item>
       </router-link>
       <router-link
         to="/personal"
         tag="div"
       >
-        <el-menu-item
-          index="3"
-          @click="openDialog"
-        >个人中心</el-menu-item>
+        <el-menu-item index="2">个人中心</el-menu-item>
       </router-link>
+      <el-dropdown
+        :hide-on-click="false"
+        trigger="click"
+        @command="selectDropMenu"
+      >
+        <div class="avatarBox">
+          <div class="avatar">
+            <img
+              v-if="userData"
+              :src="userData.avatar"
+            >
+          </div>
+        </div>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item
+            v-for="item in dropdownOption"
+            :key="item"
+            :command="item"
+          >{{item}}</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <div class="feedback">
+        <span @click="dialogVisible = true">意见反馈</span>
+        <el-dialog
+          title="意见反馈"
+          :visible.sync="dialogVisible"
+          width="50%"
+        >
+          <!-- <el-input type="textarea " v-model="feedbackText" autocomplete="off"></el-input> -->
+
+          <el-input
+            type="textarea"
+            placeholder="请输入内容"
+            v-model="feedbackText"
+            maxlength="100"
+            show-word-limit
+          >
+          </el-input>
+          <span
+            slot="footer"
+            class="dialog-footer"
+          >
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button
+              type="primary"
+              @click="dialogVisible = false"
+            >提 交</el-button>
+          </span>
+        </el-dialog>
+      </div>
     </el-menu>
     <Login
       v-if="!isLogin()"
@@ -40,17 +87,36 @@
 
 <script>
 import Login from "../components/Login";
-
+import { mapGetters, mapMutations } from "vuex";
 export default {
   components: {
     Login
   },
   data() {
     return {
-      dialogFormVisible: false
+      dialogFormVisible: false,
+      routeList: ["/compression", "/conversion", "/personal"],
+      defaultActive: "0",
+      notloggedOption: ["登录/注册"],
+      loggedOption: ["退出登录"],
+      dialogVisible: false,
+      feedbackText: ""
     };
   },
+  computed: {
+    ...mapGetters(["userData"]),
+    dropdownOption() {
+      return this.userData ? this.loggedOption : this.notloggedOption;
+    }
+  },
+  mounted() {
+    this.defaultActive = this.routeList.indexOf(this.$route.path).toString();
+    if (this.$route.path === "/") this.defaultActive = "0";
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    this.updateUserData(userData);
+  },
   methods: {
+    ...mapMutations(["updateUserData"]),
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
     },
@@ -61,7 +127,19 @@ export default {
       this.dialogFormVisible = false;
     },
     isLogin() {
-      return localStorage.getItem("token");
+      return this.userData;
+    },
+    selectDropMenu(command) {
+      if (command === "退出登录") {
+        this.logout();
+      } else if (command === "登录/注册") {
+        this.openDialog();
+      }
+    },
+    logout() {
+      this.updateUserData(null);
+      localStorage.removeItem("token");
+      localStorage.removeItem("userData");
     }
   }
 };
@@ -72,11 +150,36 @@ export default {
   .header-menu {
     display: flex;
     justify-content: center;
+    align-items: center;
     border: 0;
     .personal-center {
       align-self: flex-end !important;
     }
-    .is-active {
+    .avatarBox {
+      height: 100%;
+      outline: none;
+      cursor: pointer;
+      .avatar {
+        width: 30px;
+        height: 30px;
+        background-color: #909399;
+        border-radius: 50%;
+        overflow: hidden;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+    .feedback {
+      font-size: 14px;
+      outline: none;
+      cursor: pointer;
+      position: absolute;
+      right: 20px;
+      &:hover {
+        color: #409eff;
+      }
     }
   }
 }
